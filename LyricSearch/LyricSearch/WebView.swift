6550ede5e4dc7c7856c623b959693f3fedbc62e6
Deborah <dnewberry16@gmail.com>
2021-08-15 23:25:22 -0700

    Clean up

--- LyricSearch/LyricSearch/WebView.swift
+++ LyricSearch/LyricSearch/WebView.swift
@@ -11,11 +11,9 @@ import Combine
 
 struct WebView: UIViewRepresentable {
     let url: URL?
-    let navigationHelper = WebViewHelper()
 
     func makeUIView(context: UIViewRepresentableContext<WebView>) -> WKWebView {
         let webview = WKWebView()
-        webview.navigationDelegate = navigationHelper
 
         if let url = self.url {
             let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
@@ -30,24 +28,5 @@ struct WebView: UIViewRepresentable {
             let request = URLRequest(url: url, cachePolicy: .returnCacheDataElseLoad)
             webview.load(request)
         }
-    }
-}
-
-class WebViewHelper: NSObject, WKNavigationDelegate {
-    func webView(_ webView: WKWebView, didFinish navigation: WKNavigation!) {
-        print("webview didFinishNavigation")
-    }
-    
-    func webView(_ webView: WKWebView, didStartProvisionalNavigation navigation: WKNavigation!) {
-        print("didStartProvisionalNavigation")
-    }
-    
-    func webView(_ webView: WKWebView, didCommit navigation: WKNavigation!) {
-        print("webviewDidCommit")
-    }
-    
-    func webView(_ webView: WKWebView, didReceive challenge: URLAuthenticationChallenge, completionHandler: @escaping (URLSession.AuthChallengeDisposition, URLCredential?) -> Void) {
-        print("didReceiveAuthenticationChallenge")
-        completionHandler(.performDefaultHandling, nil)
     }
 }
