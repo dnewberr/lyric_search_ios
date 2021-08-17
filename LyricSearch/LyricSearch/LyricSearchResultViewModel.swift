87b231a3f1c19dae04e3fbdf816090af6372164a
Deborah <dnewberry16@gmail.com>
2021-08-16 22:23:27 -0700

    clean up

--- LyricSearch/LyricSearch/LyricSearchResultViewModel.swift
+++ LyricSearch/LyricSearch/LyricSearchResultViewModel.swift
@@ -8,38 +8,6 @@
 import Combine
 import SwiftUI
 
-struct LyricSearchResultView: View {
-    private var cancellableBag = Set<AnyCancellable>()
-    private let viewModel: LyricSearchResultViewModel
-    private let output: LyricSearchResultViewModel.Output
-    
-    @State private var url: URL?
-    @State private var currentError: GeniusAPIError? = nil
-
-    init(viewModel: LyricSearchResultViewModel) {
-        self.viewModel = viewModel
-        self.output = viewModel.bind()
-    }
-    
-    var body: some View {
-        VStack {
-            if let error = currentError {
-                Text(error.message)
-                Spacer()
-            } else {
-                WebView(url: url)
-            }
-        }
-        .cornerRadius(8)
-        .onReceive(output.geniusSongURLPublisher) { url in
-            self.url = url
-        }
-        .onReceive(output.errorPublisher) { error in
-            self.currentError = error
-        }
-    }
-}
-
 final class LyricSearchResultViewModel: ObservableObject {
     private let service: GeniusAPIService
     private var lastQuery: String?
